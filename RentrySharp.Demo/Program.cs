46205a62dd2b83a07bb7d4f7cf406e80cd9f76d5
Loan <naolyt@gmail.com>
2022-06-13 09:35:04 +0200

    feat(doc): add simple example

--- RentrySharp.Demo/Program.cs
+++ RentrySharp.Demo/Program.cs
@@ -4,7 +4,15 @@
 
         private static void Main(string[] args) {
 
-            // TODO: Quick demo
+            // Create a paste with a random id/password
+            Paste paste = new Paste();
+            paste.Create("Creating a paste on https://rentry.co using RentrySharp");
+            Console.WriteLine($"Created a Paste with Id={paste.Id} and Password={paste.Password} (Uri={paste.Uri})");
+
+            // Wait for the user to press enter
+            Console.WriteLine("Press ENTER to delete the Paste");
+            Console.ReadLine();
+            paste.Delete();
 
         }
 
