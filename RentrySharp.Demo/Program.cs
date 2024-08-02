d39739beecec21b8e5859d58f08710db81e26944
jgregordev <14856717+jgregordev@users.noreply.github.com>
2024-08-02 14:34:20 -0400

    Extend RentrySharp.Demo to show Edit use

--- RentrySharp.Demo/Program.cs
+++ RentrySharp.Demo/Program.cs
@@ -9,6 +9,14 @@
             paste.Create("Creating a paste on https://rentry.co using RentrySharp");
             Console.WriteLine($"Created a Paste with Id={paste.Id} and Password={paste.Password} (Uri={paste.Uri})");
 
+            //Fetch the text contents of the paste. paste.GetTextAsync is also available
+            Console.WriteLine($"New paste contents: {paste.Text}");
+
+            //Update the paste with stored credentials
+            paste.Edit(text: "Updating a paste on https://rentry.co using RentrySharp");
+
+            Console.WriteLine($"Updated paste contents: {paste.Text}");
+
             // Wait for the user to press enter
             Console.WriteLine("Press ENTER to delete the Paste");
             Console.ReadLine();
